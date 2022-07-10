d6e43af704546d224a83855a45cbe5121f32487a
Tran Hai Trieu <haitrieu2524@gmail.com>
2022-07-10 13:30:18 +0700

    25. [MVC] Model

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-const route = require("./routes")
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-// app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // Teamplate Engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/resources/views'));
 
 // Route init
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
